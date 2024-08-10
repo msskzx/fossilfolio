e5aa93f75bc9ff63e1fbb4396accac43baa5740b
Muhammad Khattab <msskzx@gmail.com>
2024-08-10 15:58:20 +0200

    added meh data

---
+++ app/meh.tsx
@@ -0,0 +1,10 @@
+export const meh = {
+    name: 'Muhammad Khattab',
+    role: 'Software Engineer',
+    intro: 'Since 2014, I have been developing projects across most of computer science domains.',
+    years: { val: 10, title: '+ Years', desc: 'of Software Development' },
+    projects: { val: 50, title: '+ Projects', desc: 'in Diverse Teams' },
+    problems: { val: 500, title: '+ Problems', desc: 'on Coding Platforms' },
+    url: '/',
+    imageUrl: '/meh.jpg',
+  }
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,10 @@
 import Image from "next/image";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faReact,
-  faPython,
-} from '@fortawesome/free-brands-svg-icons'
-
 import AnimatedCard from './animatedCard';
 import { links } from './links'
 import { topics } from './topics'
 import { highlights } from './highlights'
-
-const meh = {
-  name: 'Muhammad Khattab',
-  role: 'Software Engineer',
-  intro: 'Since 2014, I have been developing projects across most of computer science domains.',
-  years: { val: 10, title: '+ Years', desc: 'of Software Development' },
-  projects: { val: 50, title: '+ Projects', desc: 'in Diverse Teams' },
-  problems: { val: 500, title: '+ Problems', desc: 'on Coding Platforms' },
-  url: '/',
-  imageUrl: '/meh.jpg',
-}
+import { meh } from './meh'
 
 export default function Home() {
   return (
