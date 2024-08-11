f76244bbd649c25866ae39d54ba8ce0607b0b726
Muhammad Khattab <msskzx@gmail.com>
2024-08-11 15:10:04 +0200

    updated topics

--- app/topics.tsx
+++ app/topics.tsx
@@ -1,11 +1,11 @@
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import {
-    faLinux,
     faJava,
     faDocker,
-    faReact,
     faPython,
     faUnity,
+    faAws,
+    faLaravel,
 } from '@fortawesome/free-brands-svg-icons'
 
 import {
@@ -21,35 +21,35 @@ interface Topic {
 export const topics: Topic[] = [
     {
         title: 'Design Patterns',
-        desc: 'Java; Python',
+        desc: 'Java; Python; PHP; C++; Kotlin',
         icon: faJava,
     }, {
         title: 'System Design',
-        desc: 'Docker; APIs',
-        icon: faLinux,
-    }, {
-        title: 'Cloud Technologies',
-        desc: 'Kubernetes; AWS',
+        desc: 'Docker; SQL; Kubernetes; NoSQL',
         icon: faDocker,
     }, {
+        title: 'Cloud Computing',
+        desc: 'AWS; GitOps; Kafka; Airflow',
+        icon: faAws,
+    }, {
         title: 'Web Development',
-        desc: 'React; TypeScript',
-        icon: faReact,
+        desc: 'Spring; Laravel; React; TypeScript',
+        icon: faLaravel,
     }, {
         title: 'Computer Vision',
-        desc: 'Python; C++',
+        desc: 'Python; C++; OpenCV;',
+        icon: faPython,
+    }, {
+        title: 'Language Models',
+        desc: 'PyTorch; NumPy; NLTK',
         icon: faPython,
     }, {
         title: 'Augmented Reality',
         desc: 'C#; Unity',
         icon: faUnity,
     }, {
-        title: 'Language Models',
-        desc: 'Python; PyTorch',
-        icon: faPython,
-    }, {
         title: 'Parallel Computing',
-        desc: 'CUDA; C',
+        desc: 'CUDA; C; OpenMP',
         icon: faC,
     },
 ]
